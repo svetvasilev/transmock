047e14458131a53b1aff631d675e8834bf342508
cyberbyber <cyberbyber@hotmail.com>
2014-10-02 18:46:18 +0200

    Adding the remainder of the BTDF files and the Schemas project to the BizTalk solution

---
+++ BizTalkTests/Schemas/TestSchema.xsd.cs
@@ -0,0 +1,51 @@
+namespace Schemas {
+    using Microsoft.XLANGs.BaseTypes;
+    
+    
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.BizTalk.Schema.Compiler", "3.0.1.0")]
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [SchemaType(SchemaTypeEnum.Document)]
+    [Schema(@"http://Schemas.TestSchema",@"Root")]
+    [System.SerializableAttribute()]
+    [SchemaRoots(new string[] {@"Root"})]
+    public sealed class TestSchema : Microsoft.XLANGs.BaseTypes.SchemaBase {
+        
+        [System.NonSerializedAttribute()]
+        private static object _rawSchema;
+        
+        [System.NonSerializedAttribute()]
+        private const string _strSchema = @"<?xml version=""1.0"" encoding=""utf-16""?>
+<xs:schema xmlns=""http://Schemas.TestSchema"" xmlns:b=""http://schemas.microsoft.com/BizTalk/2003"" targetNamespace=""http://Schemas.TestSchema"" xmlns:xs=""http://www.w3.org/2001/XMLSchema"">
+  <xs:element name=""Root"">
+    <xs:complexType />
+  </xs:element>
+</xs:schema>";
+        
+        public TestSchema() {
+        }
+        
+        public override string XmlContent {
+            get {
+                return _strSchema;
+            }
+        }
+        
+        public override string[] RootNodes {
+            get {
+                string[] _RootElements = new string [1];
+                _RootElements[0] = "Root";
+                return _RootElements;
+            }
+        }
+        
+        protected override object RawSchema {
+            get {
+                return _rawSchema;
+            }
+            set {
+                _rawSchema = value;
+            }
+        }
+    }
+}
