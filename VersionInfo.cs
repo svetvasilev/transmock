d52942d12feeb806a26b12fbf4812c493cdbc7af
cyberbyber <cyberbyber@hotmail.com>
2014-10-16 17:57:05 +0200

    Updated the version to 0.9.5

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.9.3.*")]
-[assembly: AssemblyFileVersion("0.9.3.*")]
+[assembly: AssemblyVersion("0.9.5.*")]
+[assembly: AssemblyFileVersion("0.9.5.*")]
