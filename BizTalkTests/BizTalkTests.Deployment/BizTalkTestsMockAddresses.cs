885ffb7f2ce0800a8d2c81468288a123bb5f5a22
cyberbyber <cyberbyber@hotmail.com>
2014-11-12 00:01:37 +0100

    Added BiztalkTest projects including dynamic send port tests for BizTalk 2010

---
+++ BizTalkTests/BizTalkTests.Deployment/BizTalkTestsMockAddresses.cs
@@ -0,0 +1,37 @@
+﻿
+namespace BizTalkTests.Test {
+	public static class BizTalkTestsMockAddresses {
+		public static string BTS.OneWaySendFILE
+		{
+			get
+			{
+				return "mock://localhost/BTS.OneWaySendFILE";
+			}
+		}
+
+		public static string BTS.TwoWayTestSendWCF
+		{
+			get
+			{
+				return "mock://localhost/BTS.TwoWayTestSendWCF";
+			}
+		}
+
+		public static string BTS.OneWayReceive_FILE
+		{
+			get
+			{
+				return "mock://localhost/BTS.OneWayReceive_FILE";
+			}
+		}
+
+		public static string BTS.TwoWayTestReceive_WCF
+		{
+			get
+			{
+				return "mock://localhost/BTS.TwoWayTestReceive_WCF";
+			}
+		}
+
+	}
+}
