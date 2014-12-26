16b39cbf20de127ec1e87f57417dab00b2da098e
cyberbyber <cyberbyber@hotmail.com>
2014-12-26 09:21:01 +0100

    Added bat files for building TransMock solution from the command prompt

---
+++ BuildTransMock_Debug.bat
@@ -0,0 +1,8 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe TransMock.BTS2010.sln /nologo /t:Rebuild /p:Configuration="Debug" /p:Platform="Mixed Platforms"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe TransMock.BTS2013.sln /nologo /t:Rebuild /p:Configuration="Debug" /p:Platform="Mixed Platforms"
+
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\msbuild.exe TransMock.BTS2013R2.sln /nologo /t:Rebuild /p:Configuration="Debug" /p:Platform="Mixed Platforms"
+
+pause
