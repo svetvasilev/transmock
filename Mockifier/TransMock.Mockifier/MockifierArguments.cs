c0b515c222c600ccfa7e2f8a0752682c06ab7989
cyberbyber <cyberbyber@hotmail.com>
2015-08-07 19:12:29 +0200

    Fixed styling and code analysis issues for the Mockifier.exe assembly

---
+++ Mockifier/TransMock.Mockifier/MockifierArguments.cs
@@ -0,0 +1,193 @@
+﻿/***************************************
+//   Copyright 2014 - Svetoslav Vasilev
+
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+*****************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommandLine;
+using CommandLine.Text;
+
+namespace TransMock.Mockifier
+{
+    /// <summary>
+    /// Represents the command line arguments for the Mockifier
+    /// </summary>
+    public class MockifierArguments
+    {
+        /// <summary>
+        /// Gets or sets the path to the input bindings
+        /// </summary>
+        [Option('b', "bindings", Required = true,
+            HelpText = "Input bindings file to be processed.")]
+        public string InputBindings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path to the output bindings
+        /// </summary>
+        [Option('o', "output", Required = false,
+            HelpText = "The path to where the processed bindings file will be stored. Optional.")]
+        public string OutputBindings { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path to the output helper class
+        /// </summary>
+        [Option('c', "classOutput", Required = false,
+            HelpText = "The path to where the mocked URL helper class file will be stored. Optional.")]
+        public string OutputClass { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path to the mock mapping file
+        /// </summary>
+        [Option('m', "mockmap", Required = false,
+            HelpText = "The path to the mock mapping file.")]
+        public string MockMap { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version of BizTalk server
+        /// </summary>
+        [Option('r', "btsVersion", Required = false,
+             DefaultValue = "2013",
+            HelpText = "The BizTalk server version. Default is the latest version. Optional.")]
+        public string BtsVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the unescape mode is defined
+        /// </summary>
+        [Option('u', "unescape", Required = false,
+             DefaultValue = false,
+            HelpText = "Specifies whether the transport configuration should be unescaped. Default is false. Optional.")]
+        public bool Unescape { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the verbosity is turned on
+        /// </summary>
+        [Option('v', "verbose", DefaultValue = false,
+          HelpText = "Prints all messages to standard output.")]
+        public bool Verbose { get; set; }
+
+        /// <summary>
+        /// Gets or sets the parser state
+        /// </summary>
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        /// <summary>
+        /// Gets the description of how to use the mockifier
+        /// </summary>
+        /// <returns>A string containing the usage description</returns>
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(
+                this,
+                (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+
+        /// <summary>
+        /// Parses the parameters and evaluates which ones have been provided
+        /// </summary>
+        /// <returns>The enumeration value representing which parameters were supplied at the command prompt</returns>
+        public ParameterCombination EvaluateParametersCombination()
+        {
+            if (string.IsNullOrEmpty(this.OutputBindings) && string.IsNullOrEmpty(this.OutputClass))
+            {
+                return ParameterCombination.DefaultParams;
+            }
+            else if (!string.IsNullOrEmpty(this.OutputBindings) && string.IsNullOrEmpty(this.OutputClass))
+            {
+                if (this.BtsVersion == "2013")
+                {
+                    if (this.Unescape)
+                    {
+                        return ParameterCombination.OutputBindingsAndUnescape;
+                    }
+                    else
+                    {
+                        return ParameterCombination.OutputBindingsOnly;
+                    }
+                }
+                else
+                {
+                    if (this.Unescape)
+                    {
+                        return ParameterCombination.OutputBindingsAndBtsVersionAndUnescape;
+                    }
+                    else
+                    {
+                        return ParameterCombination.OutputBindingsAndBtsVersion;
+                    }
+                }
+            }
+            else if (string.IsNullOrEmpty(this.OutputBindings) && !string.IsNullOrEmpty(this.OutputClass))
+            {
+                if (this.BtsVersion == "2013")
+                {
+                    if (this.Unescape)
+                    {
+                        return ParameterCombination.OutputClassAndUnescape;
+                    }
+                    else
+                    {
+                        return ParameterCombination.OutputClassOnly;
+                    }
+                }
+                else
+                {
+                    if (this.Unescape)
+                    {
+                        return ParameterCombination.OutputClassAndBtsVersionAndUnescape;
+                    }
+                    else
+                    {
+                        return ParameterCombination.OutputClassAndBtsVersion;
+                    }
+                }
+            }
+            else if (!string.IsNullOrEmpty(this.OutputBindings) && !string.IsNullOrEmpty(this.OutputClass))
+            {
+                if (this.BtsVersion == "2013")
+                {
+                    if (this.Unescape)
+                    {
+                        return ParameterCombination.OutputBindingsAndClassOutputAndUnescape;
+                    }
+                    else
+                    {
+                        return ParameterCombination.OutputBindingsAndClassOutput;
+                    }
+                }
+                else
+                {
+                    if (this.Unescape)
+                    {
+                        return ParameterCombination.AllParams;
+                    }
+                    else
+                    {
+                        return ParameterCombination.OutputBindingsAndClassOutputAndBtsVersion;
+                    }
+                }
+            }
+            else
+            {
+                return ParameterCombination.NoParams;
+            }
+        }
+    }
+}
---
+++ Mockifier/TransMock.Mockifier/ParameterCombination.cs
@@ -0,0 +1,100 @@
+﻿/***************************************
+//   Copyright 2014 - Svetoslav Vasilev
+
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+*****************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransMock.Mockifier
+{
+    /// <summary>
+    /// Enumeration that depicts the combination of passed parameters
+    /// </summary>
+    public enum ParameterCombination
+    {
+        /// <summary>
+        /// Indicates no parameters were supplied
+        /// </summary>
+        NoParams,
+
+        /// <summary>
+        /// Indicates that the default parameters were supplied
+        /// </summary>
+        DefaultParams,
+
+        /// <summary>
+        /// Indicates that only the output bindings parameter was supplied
+        /// </summary>
+        OutputBindingsOnly,
+
+        /// <summary>
+        /// Indicates that only the output class parameter was supplied
+        /// </summary>
+        OutputClassOnly,
+
+        /// <summary>
+        /// Indicates that the output bindings and unescape parameters were supplied
+        /// </summary>
+        OutputBindingsAndUnescape,
+
+        /// <summary>
+        /// Indicates that the output class and unescape parameters were supplied
+        /// </summary>
+        OutputClassAndUnescape,
+
+        /// <summary>
+        /// Indicates that the output bindings and class output parameters were supplied
+        /// </summary>
+        OutputBindingsAndClassOutput,
+
+        /// <summary>
+        /// Indicates that the output bindings, class output and unescape parameters were supplied
+        /// </summary>
+        OutputBindingsAndClassOutputAndUnescape,
+
+        /// <summary>
+        /// Indicates that the output bindings and BizTalk version parameters were supplied
+        /// </summary>
+        OutputBindingsAndBtsVersion,
+
+        /// <summary>
+        /// Indicates that the output bindings, BizTalk version and unescape parameters were supplied
+        /// </summary>
+        OutputBindingsAndBtsVersionAndUnescape,
+
+        /// <summary>
+        /// Indicates that the output class and BizTalk version parameters were supplied
+        /// </summary>
+        OutputClassAndBtsVersion,
+
+        /// <summary>
+        /// Indicates that the output class, BizTalk version and unescape parameters were supplied
+        /// </summary>
+        OutputClassAndBtsVersionAndUnescape,
+
+        /// <summary>
+        /// Indicates that the output bindings, output class and BizTalk version parameters were supplied
+        /// </summary>
+        OutputBindingsAndClassOutputAndBtsVersion,
+
+        /// <summary>
+        /// Indicates that all parameters were supplied
+        /// </summary>
+        AllParams
+    }
+}
