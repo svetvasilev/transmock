1c6e9156d7fdf43170e4d7423d268d7ffcef6598
cyberbyber <cyberbyber@hotmail.com>
2018-05-23 08:17:02 +0200

    Fixes #3 - issue with wrong reference to Microsoft.ServiceModel.Channels for the BizTalk 2016 adapter. Fixes #10 - issue with setting up named pipe security identity on machines with non-english locale

--- Adapter/VersionInfo.cs
+++ Adapter/VersionInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.3.2.0")]
-[assembly: AssemblyFileVersion("1.3.276.6384")]
+[assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyFileVersion("1.3.3.6395")]
